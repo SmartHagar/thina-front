a006955a350986e064da8ebed0ccf3258bba5f5b
SmartHagar <smarthagar@gmail.com>
2024-02-02 03:37:07 +0900

    menambah loading pada login

--- src/app/(auth)/login/Form.tsx
+++ src/app/(auth)/login/Form.tsx
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Cookies from "js-cookie";
 import InputTextPassword from "@/components/input/InputTextPassword";
+import LoadingSpiner from "@/components/loading/LoadingSpiner";
 
 type Props = {};
 
@@ -101,27 +102,34 @@ const Form = (props: Props) => {
           />
         </div>
         <div className="mt-5">
-          <button
-            type="submit"
-            className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
-            onClick={handleSubmit(onSubmit)}
-          >
-            <span className="inline-block mr-2">Login</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-4 h-4 inline-block"
+          {isLoading && (
+            <div className="border flex justify-center">
+              <LoadingSpiner />
+            </div>
+          )}
+          {!isLoading && (
+            <button
+              type="submit"
+              className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+              onClick={handleSubmit(onSubmit)}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M17 8l4 4m0 0l-4 4m4-4H3"
-              />
-            </svg>
-          </button>
+              <span className="inline-block mr-2">Login</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-4 h-4 inline-block"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </button>
+          )}
         </div>
       </form>
     </div>
